f5b4b3e9ae9f8acaa9836620baed8dc58fa38893
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-10 11:23:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const word1 = prompt('inserire una parola');
+const word2 = prompt('inserire una seconda parola');
+const stamp = document.getElementById('result');
+
+console.log(word1 + word2)
