28c9982326ddad04388dedf95391a17d553783e6
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-10 11:23:35 +0100

    feat:initial commit

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ const word1 = prompt('inserire una parola');
 const word2 = prompt('inserire una seconda parola');
 const stamp = document.getElementById('result');
 
-console.log(word1 + word2)
+if (word1.length > word2.length) {
+    stamp.innerHTML(word1 + 'e piu lunga di' + word2 + 'con' + word1.length + 'lettere');
+
+} else if (word1.length < word2.length) {
+    stamp.innerHTML(word2 + 'e piu lunga di' + word1 + 'con' + word2.length + 'lettere');
+
+} else if (word1.length = word2.length) {
+    stamp.innerHTML('Entrambe le parole hanno' + word1.length + 'lettere');
+}
